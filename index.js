8c3393202e83ac9691042ccc1b66538cf32dd296
Aakash <aakashanand1995@gmail.com>
2023-04-10 00:56:47 +0530

    complete the file

--- index.js
+++ index.js
@@ -16,3 +16,17 @@
 
     var image = "images/dice" + randomNumber2 + ".png";
     document.querySelector(".dice .img2").setAttribute("src",image);
+
+    var resultVariable = result();
+
+    function result() {
+        if(randomNumber1> randomNumber2){
+            document.querySelector(".container h1").innerHTML = "Player 1 Won";
+        }
+        else if (randomNumber2> randomNumber1){
+            document.querySelector(".container h1").innerHTML = "Player 2 Won";
+        }
+        else {
+            document.querySelector(".container h1").innerHTML = "Its a Draw";
+        }
+    }
