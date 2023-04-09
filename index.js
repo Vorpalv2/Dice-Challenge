b6938a335be3bf7eed79197b2b3e7f10981fc2ed
Aakash <aakashanand1995@gmail.com>
2023-04-09 20:28:41 +0530

    updated js file

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
     var randomNumber1 = Math.round(Math.random()*6);
-    console.log(randomNumber1);
+
     if(randomNumber1 ===0){
         randomNumber1++;
     }
-    console.log(randomNumber1);
 
     var images2 = "images/dice" + randomNumber1 + ".png";
-    document.querySelector(".dice .img2").setAttribute("src",images2);
+    document.querySelector(".dice .img1").setAttribute("src",images2);
+
+
+    var randomNumber2 = Math.round(Math.random()*6);
+    if(randomNumber2 ===0){
+        randomNumber2++;
+    }
+    console.log("first number : " +randomNumber1+ " second number : "+randomNumber2+"");
+
+    var image = "images/dice" + randomNumber2 + ".png";
+    document.querySelector(".dice .img2").setAttribute("src",image);
